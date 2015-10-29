920ebe9d470fb32dd579f7e2328b2984078f205b
win777 <win777@tobesoft.com>
2015-10-29 06:15:15 +0000

    Service 테스트 케이스 업데이트

--- src/test/java/nexacro/sample/test/UserServiceTest.java
+++ src/test/java/nexacro/sample/test/UserServiceTest.java
@@ -5,41 +5,39 @@ import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 
 import javax.annotation.Resource;
-import javax.validation.ConstraintViolation;
-import javax.validation.Validation;
-import javax.validation.Validator;
-import javax.validation.ValidatorFactory;
 
 import nexacro.sample.service.UserService;
 import nexacro.sample.vo.UserVO;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.validation.BeanPropertyBindingResult;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Validator;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+
+import com.nexacro.spring.NexacroException;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = { "classpath*:spring/context-*.xml" })
+@WebAppConfiguration
+@ContextConfiguration(locations = { "classpath*:spring/context-*.xml", "file:src/main/webapp/WEB-INF/config/springmvc/dispatcher-servlet.xml" })
 public class UserServiceTest {
 
-	@Resource(name = "userService")
-	private UserService			userService;
+	@Resource
+	private UserService	userService;
 
-	private static Validator	validator;
-
-	@Before
-	public void setUp() throws Exception {
-		ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
-		validator = factory.getValidator();
-	}
-
-	@After
-	public void tearDown() throws Exception {
+	@Resource
+	private Validator	validator;
+	
+	@InitBinder
+	public void initBinder(WebDataBinder dataBinder){
+		dataBinder.setValidator(this.validator);
 	}
 
 	@Test
@@ -104,15 +102,17 @@ public class UserServiceTest {
 
 	/**
 	 * validate
-	 * @param user
-	 * @return
+	 * @param modifyList
+	 * @throws NexacroException
 	 */
-	private boolean validate(UserVO user) {
-		Set<ConstraintViolation<UserVO>> constraintViolations = validator.validate(user);
-		for (ConstraintViolation<UserVO> constraintViolation : constraintViolations) {
-			System.out.println(constraintViolation.getMessage());
+	private boolean validate(UserVO userVO) {
+		BindingResult bindingResult = new BeanPropertyBindingResult(userVO, "userVO");
+		validator.validate(userVO, bindingResult);
+		
+		if (bindingResult.hasErrors()) {
 			return false;
+		} else {
+			return true;
 		}
-		return true;
 	}
 }
